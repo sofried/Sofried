16e04c6306935745a96be4e0d9f44fc4fd2bd23a
sofried <trc5768@gmail.com>
2022-11-30 10:17:54 +0800

    Delete Douban.js

--- Douban.js
+++
@@ -1,9 +0,0 @@
-#!name=douban
-#!desc=Film
-
-[Script]
-// 茶杯狐、大师兄影视、解析网、555电影
-豆瓣电影 = type=http-response,pattern=^https://m.douban.com/movie/subject/.+,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/githubdulong/Script/master/Douban.js
-
-[Mitm]
-hostname = m.douban.com
